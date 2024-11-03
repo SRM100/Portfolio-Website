ff117dcbafdf9b9612930c6db326b578b665ff54
SRM100 <rjkm3169@gmail.com>
2024-11-03 17:54:17 +0530

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,408 @@
+export const Bio = {
+  name: "Shashwat Mishra",
+  roles: [
+    "Robotics Engineer",
+    "Data Scientist Enthusiast",
+    "Machine Learning Enthusiast",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/SHASHWAT-MISHRA-997",
+  resume:
+    "https://drive.google.com/file/d/12Ab6m0w2t8BbOcn7P0jOITW5yERlyvtn/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/sm980/",
+  twitter: "https://x.com/SHASHWATMI35662",
+  insta: "https://www.instagram.com/sm_king997/",
+
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+          },
+        ],
+      },
+      
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Flask",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Flask_logo.svg/1280px-Flask_logo.svg.png",
+      },
+      {
+        name: "Django",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlFFyY16N5NRhzoG24RqB7x8Ok2t8Bdgs-tuMsOBXS2Q&s",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "DevOps",
+    skills: [
+      {
+        name: "AWS",
+        image:
+          "https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Machine Learning",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Tenserflow",
+        image:
+          "https://static-00.iconduck.com/assets.00/tensorflow-icon-1911x2048-1m2s54vn.png",
+      },
+      {
+        name: "Keras",
+        image:
+          "https://miro.medium.com/v2/resize:fit:600/1*DKu_54iqz6C-p6ndo7rO3g.png",
+      },
+      {
+        name: "Jupyter",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Jupyter_logo.svg/1767px-Jupyter_logo.svg.png",
+      },
+      {
+        name: "Google Colab",
+        image:
+          "https://assets.stickpng.com/images/63c2e4c9c4baad4ce22bf9ef.png",
+      },
+      {
+        name: "Sk Learn Kit",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/2560px-Scikit_learn_logo_small.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://www.linkedin.com/in/sm980/overlay/1635536452522/single-media-viewer?type=IMAGE&profileId=ACoAAD18zzMBRZwNz4zraAcTrc1AYG_TEHe1ic8&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BTjDN4k0hQPau5dvslAMRGA%3D%3D",
+    role: "Data Analytics Consulting Virtual Internship",
+    company: "KPMG",
+    date: "June 2023 - July 2023",
+    desc: "Tasks that I have done in KPMG are: Data Quality Assessment, Data Insights, Data Insights and Presentation",
+    skills: [
+      "Python",
+      "Pandas",
+      "Numpy",
+      "Matplotlib",
+    ]
+  },
+  {
+    id: 1,
+    img: "https://www.linkedin.com/in/sm980/overlay/1635536468024/single-media-viewer?type=IMAGE&profileId=ACoAAD18zzMBRZwNz4zraAcTrc1AYG_TEHe1ic8&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BTjDN4k0hQPau5dvslAMRGA%3D%3D",
+    role: "Artificial Intelligence Virtual Experience Program",
+    company: "Cognizant",
+    date: "July 2023 - August 2023",
+    desc: "Tasks that I have completed in Cognizant are:\nExploratory Data Analysis\nData Modelling\nModel Building and Interpretation\nMachine Learning Production\nQuality Assurance",
+    skills: [
+      "Python",
+      "linear regression",
+      "Pandas",
+      "Numpy",
+      "Matplotlib",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://www.linkedin.com/in/sm980/overlay/experience/2304561470/multiple-media-viewer?profileId=ACoAAD18zzMBRZwNz4zraAcTrc1AYG_TEHe1ic8&treasuryMediaId=1727113892520&type=IMAGE&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BTjDN4k0hQPau5dvslAMRGA%3D%3D",
+    role: "Machine Learning Intern",
+    company: "iNeuron",
+    date: "December 2023 - January 2024",
+    desc: "• Built a machine learning model to predict the Placement status of students based on their academic and personal details. • Used various machine learning algorithms like Logistic Regression, Decision Tree, Random Forest, and XGBoost to build the model. • Achieved an accuracy of 90% using the Random Forest algorithm.",
+    skills:["python",
+            "Pandas",
+            "XGboost",
+            "Numpy",
+            "Matplotlib",
+            "Random Forest",
+            "Decision Tree",
+    ],
+    
+  },
+  {
+    id: 3,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "Project Task Assignment",
+    company: "Blackcoffer",
+    date: "October 2024 - November 2024",
+    desc: "Url-to-Article-Text-Extractor: A web application that extracts the text from the given URL and provides a summary of the article. • Implemented the web application using Flask, HTML, CSS, and JavaScript. • Used the BeautifulSoup library to extract the text from the URL. • Deployed the application on AWS.",
+    skills: [
+      "Python",
+      "Streamlit",
+      "MeautifulSoup",
+      "Selenium",
+      "HTML",
+      "CSS",
+      "Docker",
+      "AWS",
+      
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://www.google.com/imgres?q=parul%20university%20naac%20a%2B%2B&imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D100068667069073&imgrefurl=https%3A%2F%2Fwww.facebook.com%2Fpunepalregionaloffice%2F&docid=YL8kzNA4bID5yM&tbnid=40yf_KfmFFPOBM&vet=12ahUKEwjK34Cv07-JAxXFXmwGHVJQG-gQM3oECGUQAA..i&w=960&h=960&hcb=2&ved=2ahUKEwjK34Cv07-JAxXFXmwGHVJQG-gQM3oECGUQAA",
+    school: "Parul University , Vadodara",
+    date: "August 2024 - June 2026",
+
+    desc: "I am currently Pursuing a Master's degree in Robotics and Automation Engineering Parul Institute of Technology, Vadodara. ",
+    degree: "Master of Technology - MTech, Robotics and Automation Engineering",
+  },
+
+
+  {
+    id: 1,
+    img: "https://www.google.com/imgres?q=parul%20university%20naac%20a%2B%2B&imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D100068667069073&imgrefurl=https%3A%2F%2Fwww.facebook.com%2Fpunepalregionaloffice%2F&docid=YL8kzNA4bID5yM&tbnid=40yf_KfmFFPOBM&vet=12ahUKEwjK34Cv07-JAxXFXmwGHVJQG-gQM3oECGUQAA..i&w=960&h=960&hcb=2&ved=2ahUKEwjK34Cv07-JAxXFXmwGHVJQG-gQM3oECGUQAA",
+    school: "Parul University , Vadodara",
+    date: "June 2020 - June 2024",
+    grade: "First Class with Distinction",
+    desc: "I am completed a Bachelor's degree in Robotics and Automation Engineering Parul Institute of Technology, Vadodara. ",
+    degree: "Bachelor of Technology - BTech, Robotics and Automation Engineering",
+  },
+  {
+    id: 2,
+    img: "https://www.google.com/imgres?q=ambe%20school%20logo&imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D100084810623023&imgrefurl=https%3A%2F%2Fwww.facebook.com%2Fambeschool.cbse%2F&docid=hH_R92E6JV0Q7M&tbnid=oc3meyvGCkpnOM&vet=12ahUKEwj0nPuS17-JAxXObPUHHb1QFq4QM3oECBcQAA..i&w=560&h=352&hcb=2&ved=2ahUKEwj0nPuS17-JAxXObPUHHb1QFq4QM3oECBcQAA",
+    school: "Ambe School, Vadodara",
+    date: "Apr 2019 - Apr 2020",
+    grade: "First Class",
+    desc: "I completed my class 12 high school education at Ambe School, Vadodara, where I studied Science with Computer Science.",
+    degree: "CBSE(XII), Science with Computer",
+  },
+  {
+    id: 3,
+    img: "https://www.google.com/imgres?q=tree%20house%20high%20school%20logo&imgurl=https%3A%2F%2Ftreehousehighschool.com%2Fwp-content%2Fuploads%2F2023%2F09%2F1024.png&imgrefurl=https%3A%2F%2Ftreehousehighschool.com%2F&docid=vn8rT4sPEoYNuM&tbnid=b1PBSQAaePzX_M&vet=12ahUKEwint-e117-JAxVFUGcHHQdHDwgQM3oECBYQAA..i&w=1024&h=1024&hcb=2&ved=2ahUKEwint-e117-JAxVFUGcHHQdHDwgQM3oECBYQAA",
+    school: "Tree House High School, Vadodara",
+    date: "Apr 2017 - Apr 2019",
+    grade: "First Class",
+    desc: "I completed my class 10 education at Tree House High School, Vadodara, where I studied Science with Computer Application.",
+    degree: "CBSE(X), Science with Computer",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "SM-Ecommerce-Sales-Power-BI-Dashboard",
+    date: "June 2024 - July 2024",
+    description:
+      "Power BI dashboard project that provides insights into ecommerce sales data. The dashboard is designed to help ecommerce businesses track their sales performance, identify trends and make data-driven decisions.",
+    image:
+      "https://www.linkedin.com/posts/sm980_i-am-excited-to-share-my-recent-power-bi-activity-7207130769994035201-tKAI?utm_source=share&utm_medium=member_desktop",
+    library: [
+      "Power BI",
+      "CSV",
+      "Excel",
+    ],
+    category: "web app",
+    github: "git@github.com:SHASHWAT-MISHRA-997/SM-Ecommerce-Sales-PowerBi-Dashboard-Project.git",
+  },
+
+  {
+    id: 1,
+    title: "Whatsapp-Chat-Advanced-AI-Analyzer-Web-App",
+    date: "June 2024 - July 2024",
+    description:
+      "This is a WhatsApp chat analysis web app built using Streamlit, a popular Python library for building data apps. The app allows you to upload a WhatsApp chat file (in .txt format) and analyzes the data to provide various insights.",
+    image:
+      "https://www.linkedin.com/posts/sm980_streamlit-python-nlp-activity-7228509845681803264-0wyR?utm_source=share&utm_medium=member_desktop",
+    library: [
+      "Python",
+      "Pandas",
+      "Numpy",
+      "Matplotlib",
+      "Seaborn",
+      "Plotly",
+      "Streamlit",
+    ],
+    category: "web app",
+    github: "git@github.com:SHASHWAT-MISHRA-997/Whatsapp-Chat-Advanced-AI-Analyzer-Web-App.git",
+    webapp: "https://whatsapp-chat-advanced-ai-analyzer-web-app-shashwat-mishra-997.streamlit.app/",
+  },
+  
+  {
+    id: 2,
+    title: "Productivity-Enhance-Multi-Tool-Web-App",
+    date: " August 2024 - September 2024",
+    description:
+      "The Productivity-Enhance-Multi-Tool App is a powerful web application designed to streamline a wide range of digital tasks. Whether you need to manipulate PDFs, edit images, compress videos or convert various document formats this app provides a comprehensive suite of tools to enhance your productivity.",
+    image:
+      "https://www.linkedin.com/posts/sm980_webdevelopment-productivity-automation-activity-7236406631683502081-ukMd?utm_source=share&utm_medium=member_desktop",
+    library: [
+      "PyPDF2",
+      "pytesseract",
+      "pdfkit",
+      "fitz",
+      "cv2",
+      "numpy",
+      "streamlit",
+      "os",
+      "Pandas",
+      "zlib",
+    ],
+    category: "web app",
+    github: "git@github.com:SHASHWAT-MISHRA-997/Productivity-Enhance-Multi-Tool--Web-App.git",
+    webapp: "https://enhance-multi-tool--web-app-shashwat-mishra.streamlit.app/",
+  },
+
+  {
+    id: 3,
+    title: "Agriculture_Commodit_Advance_Price_Predictor -WEB-APP",
+    date: "August 2024 - September 2024",
+    description:
+      "Agriculture Commodity Price Predictor Web App! This tool provides advanced AI and ML-based predictions to help agricultural stakeholders such as farmers, businesses and government bodies analyze price trends and forecast future commodity prices in real-time.",
+    image:
+      "https://www.linkedin.com/posts/sm980_working-video-of-ai-ml-based-commodity-activity-7238897697737322498-B9Pw?utm_source=share&utm_medium=member_desktop",
+    library: ["base64", "Math", "datetime", "sklearn", "fpdf", "seaborn", "matplotlib", "streamlit"],
+    category: "web app",
+    github: "git@github.com:SHASHWAT-MISHRA-997/Agriculture_Commodity_Advance_Price_Predictor_Web_App.git",
+    webapp: "https://agriculture-commodity-advance-price-predictor-shashwat-mishra.streamlit.app/",
+  },
+  {
+    id: 4,
+    title: "Document-Insight-Bot",
+    date: "September 2024 - October 2024",
+    description:
+      "InsightBot an AI-powered tool that allows users to upload and interact with PDF documents through an intelligent chatbot. This application uses models like OpenAI GPT for answering questions based on the content of your PDFs and also offers additional analytical functionalities like word count, character count and sentiment analysis",
+    image:
+      "https://www.linkedin.com/posts/sm980_excited-to-share-my-another-project-based-activity-7243682941816520704-IQwY?utm_source=share&utm_medium=member_desktop",
+    library: [
+      "htmlTemplates",
+      "PyPdF2",
+      "openai",
+      "streamlit",
+    ],
+    category: "web app",
+    github: "git@github.com:SHASHWAT-MISHRA-997/Insight-PDF-Chat-Bot.git",
+    webapp: "https://insight-pdf-chat-bot-shashwat-mishra-997.streamlit.app/",
+  },
+
+  {
+    id: 5,
+    title: "Brain Tumor Detection",
+    date: "Jan 2023 - Mar 2023",
+    description: `💬 AI-Powered Chatbot: Interact with an advanced AI assistant to get answers on various topics.
+    
+    📄 Document Analysis: Upload and analyze content from PDFs, Word, PPTX and Excel files,
+
+    🌐 Web Content Analyzer: Extract and interact with website content by providing the URL,
+
+    🎧 Video-to-Audio Extraction: Extract and transcribe audio from video files,
+
+    🎙️ Podcast Generator: Turn PDF content into an audio podcast for easy listening,
+
+    🌍 Multilingual Translation: Translate text and documents into multiple languages,
+
+    📊 Text-to-SQL Conversion: Convert natural language queries into SQL statements,
+
+    🧑‍🏫 Research Assistant: Analyze research documents and get insights,
+
+    🧠 Sentiment & Emotion Analysis: Evaluate the sentiment and emotions from text or transcribed audio.
+    `,
+
+    image:
+      "https://www.linkedin.com/posts/sm980_excited-to-share-my-another-updated-advance-chat-assistant-activity-7251327470304448514-5stu?utm_source=share&utm_medium=member_desktop",
+    library: ["Python", "deep_translator", "Transformers", "speech_recognition", "ollama", "gTTS"],
+    category: "machine learning",
+    github: "https://github.com/SHASHWAT-MISHRA-997/Advance-Chat-Assistant",
+  },
+];
