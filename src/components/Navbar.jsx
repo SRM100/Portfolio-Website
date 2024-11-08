2d03067ad17f3cee9a8450d81ff1d5fb94a1d951
SRM100 <rjkm3169@gmail.com>
2024-11-08 15:29:33 +0530

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import { Link as LinkR } from "react-router-dom";
-import styled, { useTheme } from "styled-components";
-import { Bio } from "../data/constants";
 import { MenuRounded } from "@mui/icons-material";
+import styled, { useTheme } from "styled-components";
+import "@google/model-viewer";
 
 const Nav = styled.div`
   background-color: ${({ theme }) => theme.bg};
@@ -15,6 +14,7 @@ const Nav = styled.div`
   top: 0;
   z-index: 10;
   color: white;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
 `;
 
 const NavbarContainer = styled.div`
@@ -25,25 +25,44 @@ const NavbarContainer = styled.div`
   align-items: center;
   justify-content: space-between;
   font-size: 1rem;
+  position: relative;
 `;
 
-const NavLogo = styled(LinkR)`
-  width: 80%;
-  padding: 0 6px;
-  font-weight: 500;
-  font-size: 18px;
-  text-decoration: none;
-  color: inherit;
+const ModelViewerWrapper = styled.div`
+  width: 240px;
+  height: 240px;
+  margin-left: 20px;
+  flex-shrink: 0;
+  border-radius: 10px;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  cursor: pointer;
+  transition: transform 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(1.05);
+    filter: brightness(1.2); /* Slight light-up effect */
+  }
+
+  @media screen and (max-width: 1024px) {
+    width: 200px;
+    height: 200px;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 180px;
+    height: 180px;
+  }
 `;
 
 const NavItems = styled.ul`
-  width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
   gap: 32px;
-  padding: 0 6px;
+  padding: 0;
   list-style: none;
+  flex-grow: 1;
 
   @media screen and (max-width: 768px) {
     display: none;
@@ -54,29 +73,53 @@ const NavLink = styled.a`
   color: ${({ theme }) => theme.text_primary};
   font-weight: 500;
   cursor: pointer;
-  transition: all 0.2s ease-in-out;
   text-decoration: none;
+  padding: 8px 16px;
+  transition: all 0.3s ease;
+  border-radius: 5px;
+  position: relative;
+  overflow: hidden;
+
   &:hover {
-    color: ${({ theme }) => theme.primary};
+    background-color: transparent;
+    color: ${({ theme }) => theme.text_primary};
+    box-shadow: 0 0 10px 5px rgba(255, 255, 255, 0.8); /* Glow effect */
+    animation: rgbGlowing 1s ease-in-out infinite;
+  }
+
+  @keyframes rgbGlowing {
+    0% {
+      box-shadow: 0 0 10px 5px rgba(255, 0, 0, 0.5);
+    }
+    25% {
+      box-shadow: 0 0 10px 5px rgba(0, 255, 0, 0.5);
+    }
+    50% {
+      box-shadow: 0 0 10px 5px rgba(0, 0, 255, 0.5);
+    }
+    75% {
+      box-shadow: 0 0 10px 5px rgba(255, 255, 0, 0.5);
+    }
+    100% {
+      box-shadow: 0 0 10px 5px rgba(255, 0, 255, 0.5);
+    }
   }
 `;
 
 const ButtonContainer = styled.div`
-  width: 80%;
-  height: 100%;
   display: flex;
-  justify-content: end;
+  justify-content: flex-end;
   align-items: center;
   padding: 0 6px;
+
   @media screen and (max-width: 768px) {
     display: none;
   }
 `;
 
-const GithubButton = styled.a`
+const GithubButton = styled.button`
   border: 1px solid ${({ theme }) => theme.primary};
   color: ${({ theme }) => theme.primary};
-  justify-content: center;
   display: flex;
   align-items: center;
   border-radius: 20px;
@@ -84,84 +127,80 @@ const GithubButton = styled.a`
   padding: 10px 20px;
   font-size: 16px;
   font-weight: 500;
-  transition: all 0.6s ease-in-out;
-  text-decoration: none;
+  transition: all 0.3s ease;
+  background-color: ${({ theme }) => theme.bg};
+
   &:hover {
     background: ${({ theme }) => theme.primary};
     color: ${({ theme }) => theme.text_primary};
+    transform: scale(1.05);
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+    animation: rgbGlowing 1s ease-in-out infinite;
+  }
+
+  /* Change the color of the button */
+  background-color: #ff6347; /* Tomato color */
+  color: white;
+
+  &:hover {
+    background-color: #ff4500; /* OrangeRed color */
+    color: white;
   }
 `;
 
 const MobileIcon = styled.div`
-  height: 100%;
-  display: flex;
-  align-items: center;
-  color: ${({ theme }) => theme.text_primary};
   display: none;
   @media screen and (max-width: 768px) {
     display: block;
+    cursor: pointer;
   }
 `;
 
 const MobileMenu = styled.ul`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: start;
-  gap: 16px;
-  padding: 0 6px;
-  list-style: none;
-  width: 100%;
-  padding: 12px 40px 24px 40px;
-  background: ${({ theme }) => theme.card_light + 99};
   position: absolute;
   top: 80px;
   right: 0;
-
-  transition: all 0.6s ease-in-out;
-  transform: ${({ isOpen }) => (isOpen ? "translateY(0)" : "translateY(-100%)")};
-  border-radius: 0 0 20px 20px;
-  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
-  opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
-  z-index: ${({ isOpen }) => (isOpen ? "1000" : "-1000")};
+  width: 100%;
+  padding: 12px 40px;
+  background: ${({ theme }) => theme.card_light};
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  opacity: ${({ isOpen }) => (isOpen ? "1" : "0")};
+  transform: ${({ isOpen }) => (isOpen ? "translateY(0)" : "translateY(-20px)")};
+  transition: all 0.3s ease;
+  border-radius: 10px;
+  box-shadow: 0 4px 15px rgba(0, 0, 0, 0.2);
 `;
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isAudioStarted, setIsAudioStarted] = useState(false);
   const theme = useTheme();
-  const [currentSection, setCurrentSection] = useState('About'); // Track current section
+  const [currentSection, setCurrentSection] = useState('About');
 
-  // Function to handle section change
   const handleSectionChange = (section) => {
     setCurrentSection(section);
     const hoverAudio = document.getElementById("hover-audio");
     const vangelisAudio = document.getElementById("vangelis-audio");
 
-    // Play hover audio for all sections except About
     if (section !== 'About') {
       hoverAudio.play();
     }
 
-    // Play Vangelis audio for specific sections
     if (section === 'Projects' || section === 'Certificates') {
       vangelisAudio.play();
     }
   };
 
-  // Function to start audio playback after user interaction
   const startAudioPlayback = () => {
     const backgroundAudio = document.getElementById("background-audio");
-
-    // Play background audio on loop
     backgroundAudio.play();
     backgroundAudio.loop = true;
-
-    // Hide the button after starting the audio
     setIsAudioStarted(true);
   };
 
-  // Function to play bell audio when GitHub button is clicked
   const playBellAudio = () => {
     const bellAudio = document.getElementById("bell-audio");
     bellAudio.play();
@@ -170,7 +209,19 @@ const Navbar = () => {
   return (
     <Nav>
       <NavbarContainer>
-        <NavLogo to="/">Portfolio</NavLogo>
+        <ModelViewerWrapper>
+          <model-viewer
+            src="models/desk.glb"
+            camera-controls
+            disable-pan
+            disable-zoom
+            interaction-prompt="none"
+            min-camera-orbit="auto 55deg auto"
+            max-camera-orbit="auto 92deg auto"
+            field-of-view="35.3deg"
+            style={{ width: "100%", height: "100%" }}
+          />
+        </ModelViewerWrapper>
 
         <MobileIcon onClick={() => setIsOpen(!isOpen)}>
           <MenuRounded style={{ color: "inherit" }} />
@@ -183,6 +234,7 @@ const Navbar = () => {
           <NavLink onClick={() => handleSectionChange('Projects')} href="#Projects">Projects</NavLink>
           <NavLink onClick={() => handleSectionChange('Certificates')} href="#Certificates">Certificates</NavLink>
           <NavLink onClick={() => handleSectionChange('Education')} href="#Education">Education</NavLink>
+          <NavLink onClick={() => handleSectionChange('Contact')} href="#Contact">Contact</NavLink>
         </NavItems>
 
         {isOpen && (
@@ -193,38 +245,42 @@ const Navbar = () => {
             <NavLink onClick={() => handleSectionChange('Projects')} href="#Projects">Projects</NavLink>
             <NavLink onClick={() => handleSectionChange('Certificates')} href="#Certificates">Certificates</NavLink>
             <NavLink onClick={() => handleSectionChange('Education')} href="#Education">Education</NavLink>
-            
-            <GithubButton
-              href={Bio.github}
-              target="_Blank"
-              onClick={playBellAudio}
-              style={{
-                background: theme.primary,
-                color: theme.text_primary,
-              }}
-            >
-              Github Profile
-            </GithubButton>
+            <NavLink onClick={() => handleSectionChange('Contact')} href="#Contact">Contact</NavLink>
           </MobileMenu>
         )}
 
         <ButtonContainer>
-          <GithubButton href={Bio.github} target="_Blank" onClick={playBellAudio}>
-            Github Profile
-          </GithubButton>
+          {!isAudioStarted && (
+            <GithubButton
+              onClick={startAudioPlayback}
+              onMouseEnter={playBellAudio}
+            >
+              Start Audio
+            </GithubButton>
+          )}
         </ButtonContainer>
       </NavbarContainer>
 
-      {/* Button to start audio playback */}
-      {!isAudioStarted && (
-        <button onClick={startAudioPlayback}>Start Audio</button>
-      )}
-
-      {/* Audio Elements */}
-      <audio id="hover-audio" src="/audio/hover1.mp3" />
-      <audio id="vangelis-audio" src="/audio/Vangelis.mp3" />
-      <audio id="background-audio" src="/audio/background.mp3" loop />
-      <audio id="bell-audio" src="/audio/bell2.mp3" />
+      <audio id="background-audio" loop>
+        <source src="/audio/background.mp3" type="audio/mpeg" />
+        <source src="/audio/background.ogg" type="audio/ogg" />
+        Your browser does not support the audio element.
+      </audio>
+      <audio id="hover-audio">
+        <source src="/audio/hover-sound.mp3" type="audio/mpeg" />
+        <source src="/audio/hover-sound.ogg" type="audio/ogg" />
+        Your browser does not support the audio element.
+      </audio>
+      <audio id="vangelis-audio" loop>
+        <source src="/audio/vangelis.mp3" type="audio/mpeg" />
+        <source src="/audio/vangelis.ogg" type="audio/ogg" />
+        Your browser does not support the audio element.
+      </audio>
+      <audio id="bell-audio">
+        <source src="/audio/bell2.mp3" type="audio/mpeg" />
+        <source src="/audio/bell2.ogg" type="audio/ogg" />
+        Your browser does not support the audio element.
+      </audio>
     </Nav>
   );
 };
